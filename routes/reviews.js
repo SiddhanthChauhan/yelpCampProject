cb98027e5f61f566edbd4385997847c6e4bfd68a
SiddhanthChauhan <siddhanth.chauhan7@gmail.com>
2025-08-27 10:52:59 +0530

    Added in Authorization for all campgrounds and reviews so that only a particular user can edit or delete their own campgrounds and reviews.

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,23 +4,16 @@ const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema, reviewSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware.js');
+const { reviewSchema } = require('../schemas.js');
+const { isLoggedIn, validateReview , isReviewAuthor} = require('../middleware.js');
 
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+
 
 router.post('/',isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -28,7 +21,7 @@ router.post('/',isLoggedIn, validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:reviewId',isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
+            author: '68aaf1f118dbe42df2a7c0c5',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.floor(Math.random() * 1000)}`,
